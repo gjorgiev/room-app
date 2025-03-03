171d5a904e6d194e0a3590ca4892b75b68355312
zhivkoGrchev <z.grcev@gmail.com>
2025-03-03 19:01:14 +0100

    comp created

---
+++ src/Components/Res.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Res() {
+  return (
+    <div>
+      reservation
+    </div>
+  )
+}
+
+export default Res
---
+++ src/Components/RoomList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RoomList() {
+  return (
+    <div>
+      room list
+    </div>
+  )
+}
+
+export default RoomList
